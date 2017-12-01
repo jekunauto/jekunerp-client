fd6920bd0c64bcb25a7aa994ca3b12ee56aaae23
Wayfreem <532548957@qq.com>
2017-12-01 17:38:00 +0800

    添加 grid控件暂时

--- controller/RetailOrders.controller.js
+++ controller/RetailOrders.controller.js
@@ -1,13 +1,14 @@
 sap.ui.define([
 	"sap/ui/model/json/JSONModel",
-	"sap/ui/core/mvc/Controller",
+	"apestech/ui/erp/controller/BaseController",
 	"sap/ui/Device",
 	"sap/m/MessageToast",
-	"sap/m/SplitContainer"
-], function (JSONModel, Controller, Device, MessageToast, SplitContainer) {
+	"sap/m/SplitContainer",
+	"sap/ui/core/format/DateFormat"
+], function (JSONModel, BaseController, Device, MessageToast, SplitContainer, DateFormat) {
 	"use strict";
 
-	return Controller.extend("apestech.ui.erp.controller.retailOrders", {
+	return BaseController.extend("apestech.ui.erp.controller.retailOrders", {
 		onInit: function () {
 			//by default we always show the master
 			if (Device.system.desktop) {
@@ -18,9 +19,50 @@ sap.ui.define([
 			}
 			
 			var oData = {};
-			var oModel = new JSONModel(oData);
-        	this.getView().setModel(oModel);
+        	var oProductJson = this.initSampleDataModel();
+        	// oData = $.extend(oData, oProductJson);
+			this.getView().setModel(oProductJson);
 		},
+		initSampleDataModel : function() {
+			var oModel = new JSONModel();
+			var oDateFormat = DateFormat.getDateInstance({source: {pattern: "timestamp"}, pattern: "dd/MM/yyyy"});
+
+			jQuery.ajax(jQuery.sap.getModulePath("apestech.ui.erp.mockdata", "/products.json"), {
+				dataType: "json",
+				success: function (oData) {
+					var aTemp1 = [];
+					var aTemp2 = [];
+					var aSuppliersData = [];
+					var aCategoryData = [];
+					for (var i = 0; i < oData.ProductCollection.length; i++) {
+						var oProduct = oData.ProductCollection[i];
+						if (oProduct.SupplierName && jQuery.inArray(oProduct.SupplierName, aTemp1) < 0) {
+							aTemp1.push(oProduct.SupplierName);
+							aSuppliersData.push({Name: oProduct.SupplierName});
+						}
+						if (oProduct.Category && jQuery.inArray(oProduct.Category, aTemp2) < 0) {
+							aTemp2.push(oProduct.Category);
+							aCategoryData.push({Name: oProduct.Category});
+						}
+						oProduct.DeliveryDate = (new Date()).getTime() - (i % 10 * 4 * 24 * 60 * 60 * 1000);
+						oProduct.DeliveryDateStr = oDateFormat.format(new Date(oProduct.DeliveryDate));
+						oProduct.Heavy = oProduct.WeightMeasure > 1000 ? "true" : "false";
+						oProduct.Available = oProduct.Status == "Available" ? true : false;
+					}
+
+					oData.Suppliers = aSuppliersData;
+					oData.Categories = aCategoryData;
+
+					oModel.setData(oData);
+				},
+				error: function () {
+					jQuery.sap.log.error("failed to load json");
+				}
+			});
+
+			return oModel;
+		}
+		,
 		onBeforeRendering: function () {
 			if (Device.system.desktop && this._oSplitContainer) {
 				this._oSplitContainer.setMode("HideMode");
@@ -40,6 +82,18 @@ sap.ui.define([
 		},
 		onSubmit: function(){
 			console.log(this.getView().getModel().oData);
-		}
+		},
+		formatAvailableToObjectState : function (bAvailable) {
+			return bAvailable ? "Success" : "Error";
+		},
+
+		formatAvailableToIcon : function(bAvailable) {
+			return bAvailable ? "sap-icon://accept" : "sap-icon://decline";
+		},
+		handleDetailsPress : function(oEvent) {
+			MessageToast.show("Details for product with id " + this.getView().getModel().getProperty("ProductId", oEvent.getSource().getBindingContext()));
+		},
+		
+
 	});
 }, true);
