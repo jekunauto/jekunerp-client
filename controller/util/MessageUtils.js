f2266a59cf3b69822a0e0c93c91dfb815485f148
Wayfreem <532548957@qq.com>
2017-12-20 09:59:55 +0800

    增加消息提示控件，可以拖动功能

--- controller/util/MessageUtils.js
+++ controller/util/MessageUtils.js
@@ -10,10 +10,10 @@ sap.ui.define([
 	return UI5Object.extend("apestech.ui.erp.controller.MessageUtils", {
 
 		constructor : function (oComponent) {
-				this._oComponent = oComponent;
-				this._oModel = oComponent.getModel();
-				this._bMessageOpen = false;
-				this._sErrorText = "Sorry, a technical error occurred! Please try again later.";
+			this._oComponent = oComponent;
+			this._oModel = oComponent.getModel();
+			this._bMessageOpen = false;
+			this._sErrorText = "Sorry, a technical error occurred! Please try again later.";
 		},
 		/**
 		 * shwo the general Message
@@ -21,6 +21,7 @@ sap.ui.define([
 		showDefaultMessage: function (text) {
 			var dialog = new Dialog({
 				title: 'Default Message',
+				draggable: true,
 				type: 'Message',
 					content: new Text({
 						text: text
@@ -46,6 +47,7 @@ sap.ui.define([
 				title: 'Error',
 				type: 'Message',
 				state: 'Error',
+				draggable: true,
 				content: new Text({
 					text: text
 				}),
@@ -71,6 +73,7 @@ sap.ui.define([
 				title: 'Warning',
 				type: 'Message',
 				state: 'Warning',
+				draggable: true,
 				content: new Text({
 					text: text
 				}),
@@ -96,6 +99,7 @@ sap.ui.define([
 				title: 'Success',
 				type: 'Message',
 				state: 'Success',
+				draggable: true,
 				content: new Text({
 					text: text
 				}),
@@ -122,6 +126,7 @@ sap.ui.define([
 			var dialog = new Dialog({
 				title: 'Confirm',
 				type: 'Message',
+				draggable: true,
 				content: new Text({ text: json.text }),
 				beginButton: new Button({
 					text: 'Submit',
